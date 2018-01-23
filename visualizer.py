d578e25e5925af4e2b6fd7cb55695667bf45b39d
Miron Markowski <majron15@gmail.com>
2018-01-23 13:14:26 +0100

    proper fft

--- visualizer.py
+++ visualizer.py
@@ -35,6 +35,7 @@ class AudioStream(object):
         self.max_intensity = 0
         self.max_sig = 0
 
+        self.create_pens()
 
     def init_pg(self):
         pg.setConfigOptions(antialias=True)
@@ -42,8 +43,7 @@ class AudioStream(object):
             self.waveform = self.win.addPlot(
                 title='WAVEFORM', row=1, col=1)
         if self.spectrumFlag:
-            self.spectrum = self.win.addPlot(
-                title='SPECTRUM', row=2, col=1)
+            self.spectrum = self.win.addPlot(title='SPECTRUM', row=2, col=1)
         self.visualization = self.win.addPlot(title='VISUALIZATION', row=3, col=1)
 
 
@@ -53,7 +53,8 @@ class AudioStream(object):
         self.CHANNELS = 1
         self.RATE = 44100
         self.CHUNK = 1024
-        self.SMOOTHING = 0.75
+        self.spectrum_smoothing = 0.75
+        self.wave_smoothing = 0.3
         self.time = time
 
         self.p = pyaudio.PyAudio()
@@ -72,12 +73,18 @@ class AudioStream(object):
     def getXandFX(self):
         x = np.linspace(0, self.CHUNK / self.RATE, self.CHUNK)
         freq = np.fft.fftfreq(self.CHUNK, x[1] - x[0])
+        freq.sort()
         return x, freq
 
     def getYandFY(self):
         y = np.zeros(self.CHUNK)
         fy = np.zeros(self.CHUNK)
         return y, fy
+
+    def create_pens(self):
+        self.vis_pens = []
+        for r in range(0, 256):
+            self.vis_pens.append(pg.mkPen(color=(r, 30, 255 - r), width=4))
 
     def start(self):
         if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
@@ -100,12 +107,12 @@ class AudioStream(object):
                 self.traces[name].setData(data_x, data_y)
         else:
             if self.signalFlag and name == 'waveform':
-                self.traces[name] = self.waveform.plot(pen='c', width=3)
+                self.traces[name] = self.waveform.plot(pen='c', width=8)
                 self.waveform.setYRange(-2 ** 15, 2 ** 15, padding=0)
                 self.waveform.setXRange(0, max(self.x), padding=0.005)
             if self.spectrumFlag and name == 'spectrum':
-                pen = pg.mkPen(color=(0, 150, 0))
-                self.spectrum.setYRange(0, 10000, padding=0)
+                pen = pg.mkPen(color=(10, 200, 150), style=QtCore.Qt.DotLine) 
+                self.spectrum.setYRange(0, 2**16, padding=0)
                 self.traces[name] = self.spectrum.plot(pen=pen, width=3)
             if self.visualizationId == 1 and name == 'visualization1':
                 self.traces[name] = self.visualization.plot(pen='c')
@@ -122,7 +129,7 @@ class AudioStream(object):
                 pass
 
     def update(self):
-        self.update_spectrum()
+        self.update_signal()
         self.update_transform()
 
         if self.spectrumFlag:
@@ -140,18 +147,18 @@ class AudioStream(object):
             # ADD YOUR CODE HERE
             #
 
-    def update_spectrum(self):
+    def update_signal(self):
         wf_data = self.stream.read(self.CHUNK)
         wf_data = struct.unpack("%dh" % self.CHUNK, wf_data)
-        self.y = self.y * self.SMOOTHING
-        self.y += np.array(wf_data) * (1 - self.SMOOTHING)
+        self.y = self.y * self.wave_smoothing
+        self.y += np.array(wf_data) * (1 - self.wave_smoothing)
         if self.signalFlag:
             self.set_plotdata(name='waveform', data_x=self.x, data_y=self.y, pen='m')
 
     def update_transform(self):
         transformed = (np.abs(np.fft.fft(self.y)) * np.blackman(self.CHUNK)).clip(min=1)
-        self.fy = self.fy * self.SMOOTHING
-        self.fy += (1 - self.SMOOTHING) * transformed
+        self.fy = self.fy * self.spectrum_smoothing
+        self.fy += (1 - self.spectrum_smoothing) * transformed
         self.fy = np.where(self.fy > 11, self.fy, 1)
 
     def visualize_1(self, x, y):
@@ -165,14 +172,13 @@ class AudioStream(object):
         r = int(255 * intensity / self.max_intensity)
 
         self.max_intensity *= 0.999
-        return vx, vy, pg.mkPen(color=(r, 30, 255 - r))
+        return vx, vy, self.vis_pens[r]
 
     def visualize_2(self, vx, vy):
         new_max_sig = np.max(self.y)
         if new_max_sig > self.max_sig:
             self.max_sig = new_max_sig
-            beginning = np.argmax(self.fy[self.CHUNK // 4:self.CHUNK // 2])
-            beginning = 4 * beginning
+            beginning = np.argmax(self.fy[:self.CHUNK//2])
             rise = self.CHUNK - vy[beginning]
             vy[beginning] = self.CHUNK
             val = rise
@@ -192,7 +198,7 @@ class AudioStream(object):
 
         self.max_intensity *= 0.999
         self.max_sig *= 0.9
-        return vx, vy, pg.mkPen(color=(r, 30, 255 - r))
+        return vx, vy, self.vis_pens[r]
 
     def visualize_3(self, vx, vy):
         #
